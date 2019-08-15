8b107e041f2e38244eb7147a2bee9677225d8999
Eliza Munroe <era3munroe@gmail.com>
2019-08-15 16:20:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,10 +15,9 @@ class Song
   end
   
   def artist_name
-    self.artist.name == nil || self.artist.name
-      #puts nil
-    #else
-      #self.artist.name
-    #end
+    if self.artist
+      self.artist.name
+    else
+      nil
   end 
 end 
